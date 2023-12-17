0a403711988ba4dc96ebaa38b241c82badc99bd3
hanif.omar <hanif.omar@naazpremier.com.my>
2023-12-17 16:57:21 +0800

    feat: create forecast  at homescreen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import {
   Image,
   SafeAreaView,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -63,7 +64,7 @@ const Home = () => {
                   <TouchableOpacity
                     key={index}
                     className={`flex-row item-center border-0 py-4 px-4 mb-1 ${borderClass}`}
-                    onPress={() => handleLocation(loc)}
+                    onPress={() => handleLocation()}
                   >
                     <IconsSolid.MapPinIcon color="gray" size="20" />
                     <Text className="text-black ml-2">Selangor, Malaysia</Text>
@@ -72,6 +73,108 @@ const Home = () => {
               })}
             </View>
           )}
+        </View>
+
+        <View className="flex justify-around items-center flex-1 mx-4 mb-2">
+          <Text className="text-white font-bold text-2xl text-center">
+            Selangor,
+            <Text className="text-lg font-semibold text-gray-300">
+              Malaysia
+            </Text>
+          </Text>
+          <Image
+            source={require("../../assets/partlycloudy.png")}
+            className="w-52 h-52"
+          />
+          <View className="space-y-2">
+            <Text className="text-center text-white text-6xl font-bold ml-5">
+              24&#730;
+            </Text>
+            <Text className="text-center text-xl text-white tracking-widest">
+              Partly Cloudy
+            </Text>
+          </View>
+
+          <View className="flex-row space-x-10 mx-4">
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/sun.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22 Km</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/drop.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22 Km</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image
+                source={require("../../assets/icons/sun.png")}
+                className="h-6 w-6"
+              />
+              <Text className="text-white font-semibold text-base">22 Km</Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="mb-4 space-y-3">
+          <View className="flex-row items-center mx-5 space-x-2">
+            <Icons.CalendarIcon color="white" size="22" />
+            <Text className="text-base text-white">Daily Forecast</Text>
+          </View>
+          <ScrollView
+            horizontal
+            contentContainerStyle={{ paddingHorizontal: 15 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl  space-y-1 py-3 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../../assets/partlycloudy.png")}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">16&#730;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl  space-y-1 py-3 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../../assets/partlycloudy.png")}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">16&#730;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl  space-y-1 py-3 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../../assets/partlycloudy.png")}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">16&#730;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl  space-y-1 py-3 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../../assets/partlycloudy.png")}
+                className="h-11 w-11"
+              />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">16&#730;</Text>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
